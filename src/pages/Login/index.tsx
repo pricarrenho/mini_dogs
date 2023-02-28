bd5b6571c1369ce36b4aad8e76e4c10a6d7eb162
Priscilla Carrenho <priscillaonil@gmail.com>
2023-02-28 13:35:00 -0300

    feat: add page login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { Container } from "../../components/Container";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+
 export const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/home");
+  };
+
+  return (
+    <Container>
+      <Header />
+
+      <Input label="Usuário" name="usuario" type="text" />
+      <Input label="Senha" name="senha" type="text" />
+
+      <Button styleType="primary" onClick={handleClick}>
+        Entrar
+      </Button>
+    </Container>
+  );
 };
