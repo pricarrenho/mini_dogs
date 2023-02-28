f4b52f964f901d6c58fbbb5ba7523c5f70bbb431
Priscilla Carrenho <priscillaonil@gmail.com>
2023-02-28 11:19:48 -0300

    style: changed header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,11 @@
 import { Button } from "../Button";
-import { Container } from "../Container";
 import * as S from "./styles";
 
 export const Header = () => {
   return (
-    <Container>
-      <S.Wrapper>
-        <h1>Mini Dogs</h1>
-        <Button styleType="secondary"></Button>
-      </S.Wrapper>
-    </Container>
+    <S.Wrapper>
+      <h1>Mini Dogs</h1>
+      <Button styleType="secondary"></Button>
+    </S.Wrapper>
   );
 };
